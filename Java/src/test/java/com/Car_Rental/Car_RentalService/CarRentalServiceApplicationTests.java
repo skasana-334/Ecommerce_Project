d9fd25d1e95888270bcb344cb6506514d58234f2
skasana334 <sagarkasana482@gmail.com>
2025-01-26 12:28:06 +0530

    Added

---
+++ Java/src/test/java/com/Car_Rental/Car_RentalService/CarRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Car_Rental.Car_RentalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
